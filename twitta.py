b05c0dd121459266565ff714ea192b683bd02b2c
Matt <mkaiser@gmail.com>
2019-10-13 10:36:05 -0400

    imporved creds handling and api ref to timelines of food trucks

--- twitta.py
+++ twitta.py
@@ -1,30 +1,18 @@
 from __future__ import absolute_import, print_function
 
 import tweepy, base64, requests, pickle
-
+#for v2 : imporve handling of creds, support more operations with less reused code
 class twitter_client(object):
 
-    def __init__(self, cred_file="twitter_creds.bin"):
-
-        if cred_file:
-            with open(cred_file, 'rb') as readfile:
-                auth_list = pickle.load(readfile)
-            consumer_key = base64.decodebytes(auth_list[0])
-            consumer_secret = base64.decodebytes(auth_list[1])
-            access_token = base64.decodebytes(auth_list[2])
-            access_token_secret = base64.decodebytes(auth_list[3])
-        elif cred_file == None:
-            print("no cred file")
+    def __init__(self):
 
         self.session = requests.Session()
-
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)
-        #self.session.auth = requests.auth.HTTPBasicAuth(api)
-        #todo: add auth to session rather than function wise for bp
-    def tester(self):
         
+    def _handle_creds(self):
+        '''Reusable credential handler
+        Arguments:
+        self - instance reference
+        '''
         with open("twitter_creds.bin", 'rb') as readfile:
             auth_list = pickle.load(readfile)
         consumer_key = base64.decodebytes(auth_list[0])
@@ -34,23 +22,29 @@ class twitter_client(object):
     
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)
-        payload = (api.me().name)
-        return payload
-        '''try:
-            api.me().name.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            print("HTTP error:" + str(e))
-            return None
+        return auth
+    
+    def tester(self, account):
+        '''get_user timeline api reference
+        Arguments:
+        self - instance reference
+        accounts - twitter accounts to get tweets from 
         '''
+        auth = self._handle_creds()
+        api = tweepy.API(auth)
+        
+        payload = (api.user_timeline(account))
+        print (payload)
+        return payload
+        
         
 
 def main():
     client = twitter_client()
-    print(client.tester())
+    topDcfoodtrucks = ["pepebyjose", "LobstertruckDC", "dcslices", "DCEmpanadas", "CapMacDC", "bigcheesetruck", "TaKorean", "bbqbusdc", "hulagirltruck", "Borinquenlunchb"]
+    for foodtruck in topDcfoodtrucks:
+        client.tester(foodtruck)
 
 if __name__ == "__main__":
     main()
 
-
-topDcfoodtrucks = ["pepebyjose", "LobstertruckDC", "dcslices", "DCEmpanadas", "CapMacDC", "bigcheesetruck", "TaKorean", "bbqbusdc", "hulagirltruck", "Borinquenlunchb"] 
